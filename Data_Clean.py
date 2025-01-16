d57ccfc8692fb805cd212c66c37b0b87fe9be818
Vinoshan <vinoshan.ranjanathan01@gmail.com>
2025-01-16 03:20:52 -0500

    first commit

---
+++ Data_Clean.py
@@ -0,0 +1,76 @@
+#imports
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#function to merge rows for players who played for multiple teams in one season
+def single_row(df):
+    #list of possible team names depending on # of teams played for
+    non_teams = ["2TM", "3TM", "4TM", "5TM"]
+
+    #do nothing if only one team
+    if df.shape[0] == 1:
+        return df
+    else:  
+        #gets the rows where team name is in list above and sets it to the last team player played for in that season
+        row = df[df["Team"].isin(non_teams)]
+        row["Team"] = df.iloc[-1,:]["Team"]
+        return row
+
+
+#reads the mvp csv file and only looks at the listed columns
+mvps = pd.read_csv("csv\MVPs.csv")
+
+mvps = mvps[["Player", "Year", "Pts Won", "Pts Max", "Share"]]
+
+
+#reads the players csv file and cleans it up
+players = pd.read_csv("csv\Players.csv")
+del players["Unnamed: 0"]
+del players["Rk"]
+del players["Awards"]
+players["Player"] = players["Player"].str.replace("*", "", regex=False)
+
+
+
+
+#groups players by name and year and also applies the single_row function
+players = players.groupby(["Player", "Year"]).apply(single_row)
+
+#drops 2 unneeded index levels 
+players.index = players.index.droplevel()
+players.index = players.index.droplevel()
+
+#combines the players and mvp dataframes and fills listed NA columns with 0
+combined = players.merge(mvps, how="outer", on=["Player", "Year"])
+combined[["Pts Won", "Pts Max", "Share"]] = combined[["Pts Won", "Pts Max", "Share"]].fillna(0)
+
+
+standings = pd.read_csv("csv/Standings.csv")
+
+standings["Team"] = standings["Team"].str.replace("*", "", regex=False)
+
+nickname = {}
+
+with open("nicknames.txt", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines[1:]:
+        abbrev, name = line.replace("\n", "").split(",")
+        nickname[abbrev] = name
+
+combined["Team"] = combined["Team"].map(nickname)
+
+stats = combined.merge(standings, how="outer", on=["Team", "Year"])
+
+del stats["Unnamed: 0"]
+
+stats["GB"] = stats["GB"].str.replace("—", "0")
+stats = stats.apply(pd.to_numeric, errors = "ignore")
+
+
+stats.to_csv("player_mvp_stats.csv")
+
+highest_scoring = stats[stats["G"] > 70].sort_values("PTS", ascending=False).head(10)
+
+highest_scoring.plot.bar("Player","PTS")
+
+plt.show()
---
+++ Parse_Scrape.py
@@ -0,0 +1,125 @@
+#imports
+import pandas as pd
+from bs4 import BeautifulSoup
+from io import StringIO
+import time
+import html5lib
+from selenium import webdriver
+
+#list for years data will range
+years = list(range(1991,2025))
+
+
+"""
+Function to parse data from a web page when given
+"""
+def parser(location, tag, cleanup_class, table_id, file_name):
+
+    #for each id in table_id
+    for x in range(len(table_id)):
+        #to hold dataframes
+        temp_dataframes= []
+        temp_dataframes2 = []
+        dataframes = []
+
+        #for each year in list of years, find corresponding html page and opens it for reading
+        for year in years:
+            with open(location+"/{}.html".format(year),"r", encoding="utf-8") as f:
+                page = f.read()
+
+            #initializing a parser class
+            soup = BeautifulSoup(page, "html.parser")
+
+            #cleans up data table by removing unnecessary rows/classes in html
+            for i in range(len(cleanup_class)):
+                classes = soup.find_all(tag,class_ = cleanup_class[i])
+                for i in classes:
+                    i.decompose()
+
+            
+            if table_id[x] == "divs_standings_E":
+                 #selecting the table for MVP
+                table = soup.find(id="divs_standings_W")
+
+                #assigning the table to a variable
+                topic = pd.read_html(StringIO(str(table)))[0]
+
+                #adding a column to identify what year the table is from
+                topic["Year"] = year
+                topic["Team"] = topic["Western Conference"]
+                del topic["Western Conference"]
+                #adding the table to a list
+                dataframes.append(topic)
+
+
+            #selecting the table for given table id
+            table = soup.find(id=table_id[x])
+
+            #assigning the table to a variable
+            topic = pd.read_html(StringIO(str(table)))[0]
+
+            #adding a column to identify what year the table is from
+            topic["Year"] = year
+            if table_id[x] == "divs_standings_E":
+                topic["Team"] = topic["Eastern Conference"]
+                del topic["Eastern Conference"]
+            #adding the table to a list
+            dataframes.append(topic)
+            
+
+        #merges dataframes into one
+        topics = pd.concat(dataframes)
+
+
+        #writes dataframes to csv file
+        topics.to_csv("csv/"+file_name[x]+".csv")
+
+"""
+Function to scrape data from webpage and convert it into a locally stored HTML files
+
+Input paramters:
+
+url_start: url from where to scrape
+folder_name: name of folder for html files to be saved in
+"""
+def scraper(url_start, file_name):
+
+    #for each year in list of years
+    for year in years:
+
+        #selenium interface to interact with Google Chrome
+        driver = webdriver.Chrome()
+
+        #formats input url to specific year
+        url = url_start.format(year)
+
+        #opens the url and scrolls to the bottom
+        driver.get(url)
+        driver.execute_script("window.scrollTo(1,100000)")
+        
+        #pauses for 2 seconds to prevent time outs
+        time.sleep(2)
+
+        #gets the html webpage
+        html= driver.page_source
+
+        #stores html into folder
+        with open(file_name+"/{}.html".format(year), "w+", encoding="utf-8") as f:
+            f.write(html)
+
+
+"""
+Only run these once each, as the web page will time you out for making too many requests
+
+scraper("https://www.basketball-reference.com/leagues/NBA_{}_per_game.html", "all_players")
+scraper("https://www.basketball-reference.com/awards/awards_{}.html", "awards")
+scraper("https://www.basketball-reference.com/leagues/NBA_{}_standings.html", "standings")
+
+
+
+
+parser("all_players", 'tr', ["thead","norank"], ["per_game_stats"], ["Players"])
+parser("awards", 'tr', ["over_header"], ["mvp", "roy","dpoy", "smoy", "mip", "coy"], ["MVPs", "ROYs", "DPOYs", "6MOYs", "MIPs", "COYs"])
+parser("awards", 'tr', ["thead", "over_header"], ["leading_all_nba", "leading_all_defense", "leading_all_rookie"], ["ALL-NBA", "ALL-DEF", "ALL-ROOK"])
+parser("standings", 'tr', ["thead"], ["divs_standings_E"], ["Standings"])
+"""
